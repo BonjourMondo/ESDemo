c0e50021d675cdbb49a871cb2b56691784d9c504
“LeesangHyuk” <yxchen003@mail.nfsq.com.cn>
2019-05-21 11:22:25 +0800

    加入sql查询

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,24 @@
 package com.example.demo;
 
+import org.frameworkset.elasticsearch.ElasticSearchHelper;
+import org.frameworkset.elasticsearch.client.ClientInterface;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void getOrderDetailNum(){
+        ClientInterface clientUtil =  ElasticSearchHelper.getConfigRestClientUtil("sql/searchList.xml");
+        Map map = clientUtil.sqlObject(Map.class,"getListNum",null);
+        System.out.println(map);
+
     }
 
 }
